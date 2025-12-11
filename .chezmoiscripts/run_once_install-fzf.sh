cb222973750318f66ef84c3fb207a85208e4a120
chberreth <c.berreth@gmx.de>
2025-12-11 13:09:22 +0100

    install fzf from github not via apt

--- .chezmoiscripts/run_once_install-fzf.sh
+++ .chezmoiscripts/run_once_install-fzf.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-sudo apt update
-sudo apt install fzf
+
+
+# sudo apt update
+# sudo apt install fzf 
+
+# Install fzf from Github. The apt package doesn't create `~/.fzf.zsh` and oh-my-zsh plugin fails to handle 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --key-bindings --completion --no-update-rc
+
